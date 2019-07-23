1ff6db2efcdd10d7d042ed01d44c1ec39609f9ba
Alex Lenzen <alenzen@live.com>
2019-07-22 22:04:41 -0500

    horizontal nav bar, header with image/logo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <nav className="navigation">
+        <ul className="ul">
+          <li className="buttons">
+            <a href="#home" className="anchor">HOME</a></li>
+          <li className="buttons">
+            <a href="about" className="anchor">ABOUT</a></li>
+          <li className="buttons">
+            <a href="menu" className="anchor">MENU</a></li>
+          <li className="buttons">
+            <a href="where" className="anchor">WHERE</a></li>
+        </ul>
+      </nav>
+    <main>
+      <header className="coffee">
+        <div className="open">
+          <span className="opn">Open 6am to 5pm</span>
+        </div>
+        <div className="thecafe">
+          <h1 className="cafe">The<br></br>Cafe</h1>
+        </div>
+        <div className="address">
+          <span className="addy">16th sunrise blvd SW, 1010</span>
+        </div>
       </header>
-    </div>
+    </main> </>
   );
 }
 
